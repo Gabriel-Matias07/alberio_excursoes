df6e25a85bd2a0aec775b7489b3ce747c228e937
Gabriel-Matias07 <gabrielpereiramatias07@gmail.com>
2025-03-24 14:45:55 -0300

    autenticação e conexão feita ao back-end

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
